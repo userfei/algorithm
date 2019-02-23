88c725554e8deb0f73cc4a6c1a1dc86e889a797b
zhouximing <ximing.zhou@zuolin.com>
2019-02-23 14:23:56 +0800

    引入他人代码做参考

---
+++ src/chapter1_1/test.java
@@ -0,0 +1,13 @@
+package chapter1_1;
+
+/**
+ *
+ **/
+public class test {
+
+    public static void main(String[] args){
+        System.out.println("the sqrt root of 2.0 is " + Math.sqrt(2.0));
+        String result = String.format("%14.5s", "hello, world");
+        System.out.println(result);
+    }
+}
